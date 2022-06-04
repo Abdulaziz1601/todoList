c20ca8e9654d8dfe2451ad8de72a24a4da30ef49
Abdulaziz <abdulazizfaactory@gmail.com>
2022-06-04 11:26:02 +0500

    simplified code

--- app.js
+++ app.js
@@ -1,56 +1,55 @@
 "use strict";
-
 document.addEventListener('DOMContentLoaded', () => {
-
     const todoDB = {
         tasks: [
-            'to do the homework',
-            'Go to University'
+            "Go home",
+            "Make Bed"
         ]
     };
+
     //Selectors
     const todoInput = document.querySelector('.todo-input'),
           todoForm = document.querySelector('form'),
-          todoButton = document.querySelector('.todo-button'),
           todoContainer = document.querySelector('.todo-container');
+
     //Event Listeners
-    todoForm.addEventListener('submit', addTodo);
-    //Functions
-    function addTodo(event) {
+    todoForm.addEventListener('submit', function (event) {
         //Prevent form form submitting (Refreshing)
-        event.preventDefault(); 
+        event.preventDefault();
 
         let newTask = todoInput.value;
 
         if (newTask) {
-            if(newTask.length > 30) {
-                newTask = newTask.substr(0,30) + '...';
+            if (newTask.length > 30) {
+                newTask = newTask.substr(0, 30) + '...';
             }
             todoDB.tasks.push(newTask);
             createTodoList(todoDB.tasks, todoContainer);
         }
-        event.target.reset();   
-    }
+        event.target.reset();
+    });
+
+
+    //Functions
     function createTodoList(tasks, container) {
-        container.innerHTML = "";
+        container.innerHTML = '';
 
         tasks.forEach((element) => {
             container.innerHTML += `
-            <ul class="todo-list">
-                <li class="todo-task">${element}</li>
-                <div>
-                    <button class="delete"><i class="fas fa-trash-alt"></i></button>
-                    <button class="checked"><i class="fas fa-check"></i></button>
-                </div>
-            </ul>
+                <li class="todo-task">
+                    ${element}
+                    <div class="todo-icons">
+                        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+                        <button class="checked"><i class="fas fa-check"></i></button>
+                    </div>
+                </li>
             `;
         });
 
         document.querySelectorAll('.delete').forEach((btn, i) => {
             btn.addEventListener('click', () => {
-                btn.parentElement.remove();
+                btn.parentElement.parentElement.remove();
                 console.log(todoDB.tasks.splice(i, 1));
-                
                 createTodoList(todoDB.tasks, container);
             });
         });
